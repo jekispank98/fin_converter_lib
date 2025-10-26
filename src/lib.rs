1da8372a73f3bd623f1f7b58d0d57d0bc21b7516
jekis <jekispank98@gmail.com>
2025-10-26 21:07:19 +0300

    some changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-mod test;
-mod error;
-mod result;
-mod handler;
+pub mod test;
+pub mod error;
+pub mod result;
+pub mod handler;
 pub mod models;
+
+pub use models::csv::Csv;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
